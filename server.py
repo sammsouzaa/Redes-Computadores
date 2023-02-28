ba34773b84807536cbe22fb1e21bb09bdea24ac9
samuel souza <124898734+sammsouzaa@users.noreply.github.com>
2023-02-28 15:59:58 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,14 +7,21 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', port))
 server.listen()
 
+
+print(f"===  Servidor aguardando conexôes na porta {port} ===")
+conn, addr = server.accept()
+
+print(f'Conexão recebida de {addr}')
+    
 while True:
-    print(f"===  Servidor aguardando conexôes na porta {port} ===")
-    conn, addr = server.accept()
-    print(f'Conexão recebida de {addr}')
     
     ##recebe os dados enviados pelo cliente.
     data = conn.recv(4096)
-    print(f'Recebido: {data.decode()}')
+    ##mostra a mensagem
+    print(f'Cliente: {data.decode()}')
     
-    ##encerra a conexão
-    conn.close
+    ##envia uma mensagem ao cliente
+    msg = input(': ')
+    conn.send(msg.encode())
+    
+    conn.close ##encerra a conexão
