25f2d111b9a7088458a3e18320d3e4b80482bcf0
samuel souza <124898734+sammsouzaa@users.noreply.github.com>
2023-03-07 15:05:33 -0300

    Create client.py

---
+++ Calculadora-Natcat/client.py
@@ -0,0 +1,23 @@
+import socket
+
+port = 10299
+dest = 'localhost'
+
+##msg = 'Busquem conhecimento. Atte Et Bilu.'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print(f'=== Conectando ao servidor {dest} : {port} ===')
+client.connect((dest,port))
+
+while True:
+    ##envia a mensagem
+    msg = input(': ')
+    client.send(msg.encode())
+
+    ##recebe a mensagem enviada pelo servidor
+    msg = client.recv(4096)
+    ##mostra a mensagem
+    print(f'Servidor: {msg.decode()}')
+
+client.close ##encerra a conexão
