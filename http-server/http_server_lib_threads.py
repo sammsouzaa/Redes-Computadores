fd447d108e98e3693837e9c4c6b14ce58e321d59
sammsouzaa <samu3ifpr@gmail.com>
2023-03-20 09:50:41 -0300

    commit

--- http-server/http_server_lib_threads.py
+++ http-server/http_server_lib_threads.py
@@ -1,14 +1,20 @@
+
+##Importamos uma biblioteca que ja cria o http server
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 
 port = 8084
 addr = '0.0.0.0'
 
+##Fazemos uma class pra reponder o que o localhost vai receber
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+
+        ##Resṕonde com esse 'conteudo':
 
         content = '''
         
@@ -21,6 +27,7 @@ class Handler(BaseHTTPRequestHandler):
             <body> 
                 <h1> IFPR - Campus Cascavel </h1>
                 <p> Programando alegremente </p>
+                <p><input> hello</input> </p>
             </body> 
         </html>
         
@@ -29,13 +36,21 @@ class Handler(BaseHTTPRequestHandler):
         self.wfile.write(bytes(content, 'utf-8'))
         return
 
+##Criamos uma class somente para aplicar threads dentro do HTTPServer
+
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
+
+##Aqui indica quem vai responder, a porta e o endereço
+##Aqui dentro fica aquele (while true) que tinha no socket.
 
 def main():
     try:
         server = HTTPServer((addr, port), Handler)
         server.serve_forever()
+
+    ##Quando fecharmos, ele aparece o "Exitting server" e depois fecha
+
     except KeyboardInterrupt:
         print('Exitting server')
         server.socket.close()
